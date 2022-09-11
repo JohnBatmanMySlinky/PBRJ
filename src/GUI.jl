4fb28e029fa5d2f8aee54110b6f16a3865a6d1ea
John Myslinski <johnmyslinski@Johns-MacBook-Pro-2.local>
2022-09-11 22:38:13 +0200

    notes; if app only modifies the camera, we can build BVH and load files ONCE. create camera in test_display and pass to RENDER

--- src/GUI.jl
+++ src/GUI.jl
@@ -3,11 +3,23 @@ ENV["QSG_RENDER_LOOP"] = "basic"
 using QML
 using Qt5QuickControls2_jll
 
-
 using FileIO
 using Images
 
 include("RayTracing.jl")
+
+# render(
+#     integrator(
+#         camera,
+#         sampler,
+#         depth
+#     ),
+#     scene(
+#         lights,
+#         bvh
+#     ),
+#     minimal::bool
+# )
 
 function test_display(d::JuliaDisplay, LA_X::Int32, LA_Y::Int32, LA_Z::Int32, LF_X::Int32, LF_Y::Int32, LF_Z::Int32)
     status = RayTracing.RENDER(true, 1, 250, LA_X, LA_Y, LA_Z, LF_X, LF_Y, LF_Z)
