a165892768df5950f17f372d36f31c9cebcaabd0
John Myslinski <johnmyslinski@johns-macbook-pro.home>
2022-09-25 17:44:31 +0200

    munich re scene work

--- src/RayTracing.jl
+++ src/RayTracing.jl
@@ -37,6 +37,7 @@ include("shapes/shape.jl")
 include("shapes/sphere.jl")
 include("shapes/triangle.jl")
 include("shapes/rectangles.jl")
+include("shapes/box.jl")
 include("math_utils.jl")
 include("rand_utils.jl")
 include("accelerators/bvh_naive.jl")
@@ -63,6 +64,7 @@ include("materials/plastic.jl")
 include("materials/mirror.jl")
 include("textures/constant.jl")
 include("textures/image.jl")
+include("textures/procedural.jl")
 include("lights/light.jl")
 include("lights/area.jl")
 include("lights/point.jl")
@@ -72,15 +74,22 @@ include("integrators/whitted.jl")
 include("handy_prints.jl")
 include("obj_reader.jl")
 
-function make_your_scene()
-    floor_transform = Translate(Pnt3(0, 0, 0))
-    floor = XZRectangle(
-        floor_transform, 
-        Pnt2(-300, 300),
-        Pnt2(-300, 300),
-        0.0,
-        false,
-        false
+function test_integrate()
+    mat_white = Matte(
+        ConstantTexture(Vec3(1, 1, 1)),
+        ConstantTexture(Vec3(0, 0, 0)),
+        nothing
+    )
+
+    mat_blue = Matte(
+        ConstantTexture(Vec3(0, 0, 1)),
+        ConstantTexture(Vec3(0, 0, 0)),
+        nothing
+    )
+    mat_green = Matte(
+        ConstantTexture(Vec3(0, 1, 0)),
+        ConstantTexture(Vec3(0, 0, 0)),
+        nothing
     )
 
     mat_concrete = Matte(
@@ -89,17 +98,63 @@ function make_your_scene()
         ImageTexture("../ref/Stone_Floor_007_basecolor_edit.jpg")
     )
 
+    floor_transform = Translate(Pnt3(0, 0, 0))
+    floor = XZRectangle(
+        floor_transform, 
+        Pnt2(-300, 300),
+        Pnt2(-300, 300),
+        0.0,
+        nothing,
+        false,
+        false
+    )
+
+    ceiling_transform = Translate(Pnt3(0,170,0))
+    ceiling = XZRectangle(
+        ceiling_transform,
+        Pnt2(-300,300),
+        Pnt2(-300,300),
+        0.0,
+        CircleProceduralTexture(Pnt2(.5, .5), .25, Pnt3(1,1,1), Pnt3(0,0,0)),
+        true,
+        false
+    )
+
+    pillar_width_1 = 100
+    pillar_width_2 = 25
+    pillar_transform = RotateY(-12.0)
+    pillar1 = Box(
+        pillar_transform,
+        Pnt3(-pillar_width_1, -50, -pillar_width_2),
+        Pnt3(pillar_width_1, 300, pillar_width_2),
+        mat_blue
+    )
+    pillar2 = Box(
+        pillar_transform,
+        Pnt3(-pillar_width_2, -50, -pillar_width_1),
+        Pnt3(pillar_width_2, 300, pillar_width_1),
+        mat_green
+    )
+
     # vector of primitives
     primitives = Primitive[]
 
-    # add floor
+    # add box
+    primitives = vcat(primitives, pillar1, pillar2)
+
+    # add floor & ceiling
     push!(primitives, Primitive(floor, mat_concrete, nothing))
+    push!(primitives, Primitive(ceiling, mat_concrete, nothing))
+
 
     # Lights
     lights = Light[]
 
     # instantiate an area light
-    for t in [Translate(Pnt3(0, 100, 0)), Translate(Pnt3(250, 100, 0)), Translate(Pnt3(-250, 100, 0))]
+    for t in [
+        Translate(Pnt3(250, 100, 0)), 
+        Translate(Pnt3(-250, 100, 0))
+    ]
         light_orb = Sphere(
             ShapeCore(t, Inv(t), false, false),
             20.0
@@ -117,22 +172,14 @@ function make_your_scene()
     @time bvh = BVH(primitives)
     print("Done building BVH\n")
 
-    # instantiate an env light
-    env_light = InfinteLight(bvh, Translate(Vec3(0,0,0)), Translate(Vec3(0,0,0)), Spectrum(.5,.5,.5), "../ref/parking_lot.jpg")
-    push!(lights, env_light)
+    # print_BVH_bounds(BVH)
 
-    # Instantiate Scene
-    scene = Scene(lights, bvh)
-    return scene
-end
-
-function RENDER(scene::Scene, minimal::Bool, n_samples::Int64, dim::Int64, LA_X::Int32, LA_Y::Int32, LA_Z::Int32, LF_X::Int32, LF_Y::Int32, LF_Z::Int32)
     # Instantiate a Filter
     filter = BoxFilter(Pnt2(.5, .5))
 
     # Instantiate a Film
     film = Film(
-        Pnt2(dim, dim),
+        Pnt2(250, 250),
         Bounds2(Pnt2(0,0), Pnt2(1,1)),
         filter,
         1.0,
@@ -141,22 +188,31 @@ function RENDER(scene::Scene, minimal::Bool, n_samples::Int64, dim::Int64, LA_X:
     )
 
     # Instantiate a Camera
-    look_from = Pnt3(LF_X, LF_Y, LF_Z)
-    look_at = Pnt3(LA_X, LA_Y, LA_Z) # TODO something is off here....
+    look_from = Pnt3(800, 100, 800)
+    look_at = Pnt3(300, -200, 0) # TODO something is off here....
     up = Vec3(0, -1, 0)
     screen = Bounds2(Pnt2(-1, -1), Pnt2(1, 1))
     C = PerspectiveCamera(LookAt(look_from, look_at, up), screen, 0.0, 1.0, 0.0, 1e6, 175.0, film)
 
     # Instantiate a Sampler
-    S = StratifiedSampler(n_samples, n_samples, n_samples, true)
+    S = StratifiedSampler(4, 4, 4, true)
+
     print("Using " * num2str(S.samples_per_pixel) * " samples per pixel")
+
+    # instantiate an env light
+    env_light = InfinteLight(bvh, Translate(Vec3(0,0,0)), Translate(Vec3(0,0,0)), Spectrum(.5,.5,.5), "../ref/parking_lot.jpg")
+    push!(lights, env_light)
+
+    # Instantiate Scene
+    scene = Scene(lights, bvh)
     
     # Instantiate an Integrator
     I = WhittedIntegrator(C, S, 25)
 
-    @time render(I, scene, minimal)
-    return true
+    render(I, scene)
 end
 
+
+@time test_integrate()
 
 end
