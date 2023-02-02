400eeb25745ac15b9ba1e0db2957d85445a408c0
Myslinski <n372361b@munichre.com>
2023-02-02 17:23:02 +0000

    oh yeah dont forget this file

---
+++ src/lights/visibility.jl
@@ -0,0 +1,28 @@
+struct VisibilityTester
+    p0::Interaction
+    p1::Interaction
+end
+
+function unoccluded(vt::VisibilityTester, scene::BVHAccel)::Bool
+    check = intersect_p(scene, spawn_shadow_ray(vt.p0, vt.p1))
+    return !check
+end
+
+function tr(vt::VisibilityTester, scene::BVHAccel, sampler::AbstractSampler)::Spectrum
+    ray = spawn_shadow_ray(vt.p0, vt.p1)
+    Tr = Spectrum(1.0)
+    while true
+        check, t, isect = intersect!(scene, ray)
+        if check && !(isect.primitive.material isa Nothing)
+            return Spectrum(0.0)
+        end
+
+        # JOHN HACK: skipping medium check
+
+        if !check
+            break
+        end
+        ray = spawn_ray(isect, p1)
+    end
+    return Tr
+end
