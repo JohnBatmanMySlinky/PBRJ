5d79166f26f6fcc3a45590150b66bd6a4de7b674
John Myslinski <jbmyslinski@gmail.com>
2023-08-29 20:31:28 +0200

    goursat f so ugly, slight refactor

--- src/shapes/implicit_surfaces/goursat.jl
+++ src/shapes/implicit_surfaces/goursat.jl
@@ -18,15 +18,25 @@ struct GoursatSurface <: ImplicitSurface
 end
 
 function f(g::GoursatSurface, t::Float64, ray::AbstractRay)::Float64
-    return f(g, at(ray, t))
+    pp = at(ray, t)
+    return f(a, b, magic, pp)
 end
 
 function f(g::GoursatSurface, pp::Pnt3)::Float64
+    return f(g.a, g.b, g.magic, pp)
+end
+
+function f(a::Float64, b::Float64, magic::Float64, ray::AbstractRay, t::Float64)::Float64
+    pp = at(ray, t)
+    return f(a, b, magic, pp)
+end
+
+function f(a::Float64, b::Float64, magic::Float64, pp::Pnt3)::Float64
     squared_term = pp.x^2 + pp.y^2 + pp.z^2
     return pp.x^4 + pp.y^4 + pp.z^4 + 
-            g.a * squared_term^2 +
-            g.b * squared_term +
-            g.magic
+            a * squared_term^2 +
+            b * squared_term +
+            magic
 end
 
 function goursat_bounding_sphere_radius(a::Float64, b::Float64, magic::Float64)::Float64
@@ -44,17 +54,6 @@ function goursat_bounding_sphere_radius(a::Float64, b::Float64, magic::Float64):
     # WARNING
     # this method fails on the wolfram case of a=-1, b=1, c=-1
     # ----------------------------------
-
-    # TODO
-    # ugh annoying I have to instantiate this twice...
-    function f(r::AbstractRay, t::Float64, a::Float64, b::Float64, magic::Float64)::Float64
-        pp = at(ray, t)
-        squared_term = pp.x^2 + pp.y^2 + pp.z^2
-        return pp.x^4 + pp.y^4 + pp.z^4 + 
-                a * squared_term^2 +
-                b * squared_term +
-                magic
-    end
 
     # test case 1: the corner
     r = Ray(Pnt3(0,0,0), Vec3(-1.0, -1.0, -1.0), 0, typemax(Float64))
