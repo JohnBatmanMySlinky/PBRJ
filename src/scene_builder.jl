f53a97bc400ce6c573045c5856a4d5b7dee071a0
John Myslinski <jbmyslinski@gmail.com>
2023-09-29 10:06:39 -0400

    julia scene instant

--- src/scene_builder.jl
+++ src/scene_builder.jl
@@ -5,6 +5,7 @@ scene 3: dragon
 scene 4: cornell box
 scene 5: soft bodies
 scene 6: goursat
+scene 7: julia logo
 """
 
 function build_scene(parsed_args::Dict)::Tuple{AbstractIntegrator, Scene}
@@ -1124,6 +1125,58 @@ function build_scene(parsed_args::Dict)::Tuple{AbstractIntegrator, Scene}
             )
             push!(primitives, Primitive(goursat, col, nothing))
         end
+
+        # instantiate accelerator
+        print("\nThere are " * num2str(length(primitives)) * " objects in the scene, building BVH\n")
+        @time bvh = BVH(primitives)
+        print("Done building BVH\n")
+
+        # Instantiate a Filter
+        filter = BoxFilter(Pnt2(.5, .5))
+
+        # Instantiate a Film
+        film = Film(
+            Pnt2(parsed_args["image-dim"], parsed_args["image-dim"]),
+            Bounds2(Pnt2(parsed_args["crop-window"][1], parsed_args["crop-window"][2]), Pnt2(parsed_args["crop-window"][3], parsed_args["crop-window"][4])),
+            filter,
+            1.0,
+            1.0,
+            parsed_args["file-name"]
+        )
+
+        # Instantiate a Camera
+        look_from = Pnt3(10, 15, 5)
+        look_at = Pnt3(0, 0, 0)
+        up = Vec3(0, -1, 0)
+        screen = Bounds2(Pnt2(-1, -1), Pnt2(1, 1))
+        C = PerspectiveCamera(LookAt(look_from, look_at, up), screen, 0.0, 1.0, 0.0, 1e6, 30.0, film)
+
+        # Instantiate a Sampler
+        S = StratifiedSampler(parsed_args["samples-per-pixel"], parsed_args["jitter"])
+        print("Using " * num2str(S.samples_per_pixel) * " samples per pixel\n")
+        
+        # Instantiate Scene
+        print("There are " * num2str(length(lights)) * " lights in the scene\n")
+        scene = Scene(lights, bvh)
+        
+        # Instantiate an Integrator
+        I = BDPTIntegrator(C, S, parsed_args["max-depth"])
+        return I, scene
+    elseif parsed_args["scene-number"] == 7
+        primitives = Primitive[]
+        lights = Light[]
+
+        # MATERIALS
+        mat_gray = Matte(
+            ConstantTexture(spectrum_from_float(.4, .4, .4)),
+            ConstantTexture(spectrum_from_float(0.0, 0.0, 0.0)),
+            nothing
+        )
+        mat_julia_green = Matte(
+            ConstantTexture(spectrum_from_float(0.05, 0.05, .9)),
+            ConstantTexture(spectrum_from_float(0.0, 0.0, 0.0)),
+            nothing
+        )
 
         # instantiate accelerator
         print("\nThere are " * num2str(length(primitives)) * " objects in the scene, building BVH\n")
