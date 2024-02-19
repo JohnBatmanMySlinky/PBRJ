9972ad470ece40430f074bf671ccc4ef7895f816
John Myslinski <jbmyslinski@gmail.com>
2024-02-19 18:37:37 -0500

    fast owen

--- src/samplers2/low_discrepancy.jl
+++ src/samplers2/low_discrepancy.jl
@@ -73,3 +73,12 @@ function mix_bits(v::Union{Int, UInt})::Union{Int, UInt}
     v ⊻= (v >> 33)
     return v
 end
+
+function reverse_bits_32(n::UInt32)::UInt32
+    n = (n << 16) | (n >> 16)
+    n = ((n & 0x00ff00ff) << 8) | ((n & 0xff00ff00) >> 8)
+    n = ((n & 0x0f0f0f0f) << 4) | ((n & 0xf0f0f0f0) >> 4)
+    n = ((n & 0x33333333) << 2) | ((n & 0xcccccccc) >> 2)
+    n = ((n & 0x55555555) << 1) | ((n & 0xaaaaaaaa) >> 1)
+    return n;
+end
--- src/samplers2/randomizers.jl
+++ src/samplers2/randomizers.jl
@@ -4,3 +4,20 @@ end
 function (r::NoRandomizer)(a::UInt32)::UInt32
     return a
 end
+
+struct FastOwenRandomizer <: Randomizer
+    seed::UInt32
+    function FastOwenRandomizer()
+        return new(UInt32(15689))
+    end
+end
+
+function (r::FastOwenRandomizer)(v::UInt32)::UInt32
+    v = reverse_bits_32(v)
+    v ⊻= v * 0x3d20adea
+    v += r.seed
+    v *= (r.seed >> 16) | 1
+    v ⊻= v * 0x05526c56
+    v ⊻= v * 0x53a22864
+    return reverse_bits_32(v)
+end
