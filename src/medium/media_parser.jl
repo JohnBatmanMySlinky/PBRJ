5c3a8fc3d00d6d8c3a0ddbed343cc006b48472f4
John Myslinski <jbmyslinski@gmail.com>
2024-03-16 21:38:06 +0100

    janky ass parser

---
+++ src/medium/media_parser.jl
@@ -0,0 +1,20 @@
+function parse_media(fpath::String)::Tuple{Int64, Int64, Int64, Vector{Float64}}
+    f = open(fpath, "r")
+    s = read(f, String)
+
+    s = replace(replace(s, "\n" => ""), "\t"=>"")
+
+    # JOHN HAKC
+    # this isn't brittle at all
+    nx = parse(Int64, match(r"[0-9].*[0-9]", match(r"integer nx.*?integer", s).match).match)
+    ny = parse(Int64, match(r"[0-9].*[0-9]", match(r"integer ny.*?integer", s).match).match)
+    nz = parse(Int64, match(r"[0-9].*[0-9]", match(r"integer nz.*?point", s).match).match)
+    
+    d = Float64[parse(Float64, x) for x in split(strip(replace(split(match(r"density.*", s).match, "[")[2], "]"=>"")), " ")]
+
+    @assert nx * ny * nz == length(d)
+
+    return nx, ny, nz, d
+end
+
+parse_media("/Users/johnmyslinski/Documents/pbrt-v3-scenes/cloud/geometry/density_render.70.pbrt")
