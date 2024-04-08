141faaaa2985c777c6cad7b08edbaf5dfc5c2d43
John Myslinski <jbmyslinski@gmail.com>
2024-04-07 20:31:58 -0400

    heinous

--- src/mip_map.jl
+++ src/mip_map.jl
@@ -22,7 +22,7 @@ function resample_weights(old_res::Int64, new_res::Int64)::Vector{ResampleWeight
 		)
 		weight /= sum(weight)
 		wt[i+1] = ResampleWeight(first_texel, weight)
-		@info "ResampleWeights: $(i) $(first_texel) $(weight)"
+		# @info "ResampleWeights: $(i) $(first_texel) $(weight)"
 	end
 	return wt
 end
@@ -49,13 +49,13 @@ struct MIPMap
 			res_pow_2 = Pnt2(round_up_pow2(Int64(resolution.x)), round_up_pow2(Int64(resolution.y)))
 			@info "Resampling MIPMap from $(resolution) to $(res_pow_2). Ratio = $((res_pow_2.x * res_pow_2.y)/(resolution.x * resolution.y))"
 
-			i = 0
-			for x in 1:resolution.x
-				for y in 1:resolution.y
-					i += 1
-					@info "MIPMAP: ($(x), $(y)) = $(i) = $(data[i])\n"
-				end
-			end
+			# i = 0
+			# for x in 1:resolution.x
+			# 	for y in 1:resolution.y
+			# 		i += 1
+			# 		@info "MIPMAP: ($(x), $(y)) = $(i) = $(data[i])\n"
+			# 	end
+			# end
 			
 			# Resample image in $s$ direction
 			s_weights = resample_weights(Int64(resolution.x), Int64(res_pow_2.x))
@@ -77,19 +77,20 @@ struct MIPMap
 						end
 						
 						if (orig_s >= 0) && (orig_s < resolution.x)
-							@info "MIPMAP READ: $(t * resolution.x + orig_s) = $(data[Int64(t * resolution.x + orig_s + 1)])"
+							# @info "MIPMAP READ: $(s), $(t), $(t * resolution.x + orig_s) = $(data[Int64(t * resolution.x + orig_s + 1)])"
 							resampled_image[Int64(t * res_pow_2.x + s + 1)] += s_weights[s+1].weight[j+1] * data[Int64(t * resolution.x + orig_s + 1)]
 						end
 					end
 				end
 			end
-			@assert false # enough for today
 			
 			# resample image in $t$ direction
-			t_weights = resample_weights(resolution.y, res_pow_2.y)
-			for s in 0:res_pow_2.x
-				for t in 0:res_pow_2.y
-					for j in 0:4
+			t_weights = resample_weights(Int64(resolution.y), Int64(res_pow_2.y))
+			for s in 0:Int64(res_pow_2.x - 1)
+				work_data = Vector{typeof(data[1])}(undef, Int64(res_pow_2.y))
+				for t in 0:Int64(res_pow_2.y - 1)
+					work_data[t+1] = data[1] * 0.0 # BIG BRAIN JOHN HACK
+					for j in 0:3
 						offset = t_weights[t+1].first_texel + j
 						if wrap_mode == Int8(0) # repeat
 							offset = offset % resolution.y
@@ -100,27 +101,35 @@ struct MIPMap
 						end
 						
 						if (offset >= 0) && (offset < resolution.y)
-							work_data[t+1] += t_weights[t+1].weights[j+1] * resampled_image[offset * res_pow_2.x + s + 1]
+							@info "MIPMAP READ: $(s), $(t), $(offset * res_pow_2.x + s) = $(resampled_image[Int64(offset * res_pow_2.x + s + 1)])"
+							work_data[t+1] += t_weights[t+1].weight[j+1] * resampled_image[Int64(offset * res_pow_2.x + s + 1)]
 						end
 					end
 				end
-				for t in 0:res_pow_2.y
-					resampled_image[t * res_pow_2.x + s + 1] = clamp(work_data, 0, 1)
+				for t in 0:Int64(res_pow_2.y - 1)
+					resampled_image[Int64(t * res_pow_2.x + s + 1)] = clamp.(work_data[t+1], 0, 1)
 				end
 			end
 		end
 		
 		# initialize levels of MIPMap from image
-		n_levels = 1 + log_2_int(max.(resolution))
+		n_levels = 1 + Int64(floor(log(maximum(resolution))))
+		@info "N LEVELS: $(n_levels)" 
+
+		pyramid = Vector{Matrix{typeof(data[1])}}(undef, n_levels)
+		pyrsize = Vector{Pnt2}(undef, n_levels) # JOHN HACK oh god i hope this doesnt bite me later
 		
 		# Initialize most detailed level of MIPMap
-		pyramid[1] = resampled ? resampled_image : data
-		
-		for i in 1:levels
+		pyrsize[1] = resampled ? res_pow_2 : resolution
+		pyramid[1] = reshape(resampled ? resampled_image : data, (Int64(pyrsize[1].x), Int64(pyrsize[1].y))) # yeehaw
+		# JOHN HACK: this is big brain shit but in the bad way
+
+		for i in 1:(n_levels-1)
 			# Initialize $i$th MIPMap level from $i-1$st level
-			s_res = max(1, u_size(pyramid[i-1+1])/2)
-			t_res = max(1, v_size(pyramid[i-1+1])/2)
-			pyramid[i+1] = zeros(s_res, t_res)
+			s_res = Int64(max(1, pyrsize[i-1+1].x/2))
+			t_res = Int64(max(1, pyrsize[i-1+1].y/2))
+			pyramid[i+1] = zeros(typeof(data[1]), s_res, t_res)
+			pyrsize[i+1] = Pnt2(s_res, t_res)
 
 			for t in 0:t_res
 				for s in 0:s_res
