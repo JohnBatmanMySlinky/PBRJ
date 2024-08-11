bd657c49856eeca3dcf32205a87311577b8889f7
John Myslinski <jbmyslinski@gmail.com>
2024-08-11 19:58:13 -0400

    finishing touches

--- src/scene_builder.jl
+++ src/scene_builder.jl
@@ -2015,7 +2015,7 @@ function build_scene(parsed_args::Dict)::Tuple{AbstractIntegrator, Scene}
         C = PerspectiveCamera(LookAt(look_from, look_at, up), screen, 0.0, 1.0, 0.0, 1e6, 46.07, film)
 
         # Instantiate a Sampler
-        S = StratifiedSampler(parsed_args["samples-per-pixel"], parsed_args["jitter"])
+        S = ZSobolSampler(parsed_args["samples-per-pixel"], Pnt2(parsed_args["image-dim"], parsed_args["image-dim"]), Int8(2))
         print("Using " * num2str(S.samples_per_pixel) * " samples per pixel\n")
         
         # Instantiate Scene
