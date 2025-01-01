63ca2f5306c7f2db20f2ea7782fffab850937849
John Myslinski <jbmyslinski@gmail.com>
2025-01-01 15:43:48 -0500

    well looks like crap

--- src/medium2/phase_functions.jl
+++ src/medium2/phase_functions.jl
@@ -12,14 +12,15 @@ function (hg::HenyeyGreenstein)(wo::Vec3, wi::Vec3)::Float64
 end
 
 function sample_p(hg::HenyeyGreenstein, wo::Vec3, u::Pnt2)::Tuple{Float64, Vec3, Float64}
-    if abs(hg.g) < 1e-3
+    g = clamp(hg.g, -.99, .99)
+
+    if abs(g) < 1e-3
         cos_theta = 1.0 - 2.0 * u[0+1]
     else
-        sqr_term = (1.0 - hg.g^2) / (1.0 - hg.g + 2.0 * hg.g * u[0+1])
-        cos_theta = (1.0 + hg.g^2 - sqr_term) / (2.0 * hg.g)
+        cos_theta = -1.0/ (2.0 * g) * (1.0 + g^2 - ((1.0 - g^2) / (1.0 + g - 2.0 * g * u[0+1]))^2)
     end
 
-    sin_theta = sqrt(max(0.0, 1.0 - cos_theta^2))
+    sin_theta = safe_sqrt(1.0 - cos_theta^2)
     phi = 2.0 * pi * u[1+1]
     wo, v1, v2 = orthonormal_basis(wo)
     wi = spherical_direction(sin_theta, cos_theta, phi, v1, v2, wo)
