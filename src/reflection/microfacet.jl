1caee8beec6380729a4af481c0682f8d59decde7
John Myslinski <jbmyslinski@gmail.com>
2025-02-01 15:06:06 -0500

    ok well solving metal....

--- src/reflection/microfacet.jl
+++ src/reflection/microfacet.jl
@@ -25,3 +25,28 @@ function f(mr::MicrofacetReflection, wo::Vec3, wi::Vec3)
     F = mr.fresnel(dot(wi, face_forward(wh, Vec3(0,0,1))))
     return mr.R * D(mr.distrib, wh) * G(mr.distrib, wo, wi) * F / (4.0 * cos_theta_i * cos_theta_o)
 end
+
+function sample_f(bxdf::MicrofacetReflection, wo::Vec3, u::Pnt2, type::UInt8=BSDF_ALL)::Tuple{Vec3, Spectrum, Float64, Maybe{UInt8}}
+    @info "METALDEBUG: sample_f: wo: $wo"
+
+    # Sample microfacet orientation $\wh$ and reflected direction $\wi$
+    if (wo.z == 0) 
+        return Vec3(0, 0, 0), spectrum_from_float(0.0), 0.0, nothing
+    end
+    wh = sample_wh(bxdf.distrib, wo, u)
+    @info "METALDEBUG: sample_f: wh: $wh"
+    if (dot(wo, wh) < 0) 
+        return Vec3(0, 0, 0), spectrum_from_float(0.0), 0.0, nothing   # Should be rare
+    end
+    wi = reflect(wo, wh)
+    @info "METALDEBUG: sample_f: wi: $wi"
+    if (!same_hemisphere(wo, wi)) 
+        return Vec3(0, 0, 0), spectrum_from_float(0.0), 0.0, nothing
+    end
+
+    # Compute PDF of _wi_ for microfacet reflection
+    pdf_val = pdf(bxdf.distrib, wo, wh) / (4.0 * dot(wo, wh))
+    @info "METALDEBUG: sample_f: pdf_val: $pdf_val"
+    f_val = f(bxdf, wo, wi)
+    return wi, f_val, pdf_val, nothing
+end
--- src/reflection/microfacet_distributions.jl
+++ src/reflection/microfacet_distributions.jl
@@ -70,6 +70,107 @@ function roughness_to_alpha(roughness::Float64)
     return 1.62142 + 0.819955 * x + 0.1734 * x^2 + 0.0171201 * x^3 + 0.000640711 * x^4
 end
 
+function sample_wh(md::TrowbridgeReitzDistribution, wo::Vec3, u::Pnt2)::Vec3
+    if (!md.sample_visible_area)
+        cosTheta = 0
+        phi = (2 * pi) * u.y
+        if (md.alphax == md.alphay) 
+            tanTheta2 = md.alphax * md.alphax * u.x / (1.0 - u.x)
+            cosTheta = 1.0 / sqrt(1.0 + tanTheta2)
+        else
+            phi = atan(alphay / alphax * tan(2 * pi * u.y + .5 * pi))
+            if (u.y > .5) 
+                phi += pi
+            end
+            sinPhi = sin(phi)
+            cosPhi = cos(phi)
+            alphax2 = md.alphax * md.alphax
+            alphay2 = md.alphay * md.alphay
+            alpha2 = 1.0 / (cosPhi * cosPhi / alphax2 + sinPhi * sinPhi / alphay2)
+            tanTheta2 = alpha2 * u.x / (1.0 - u.x)
+            cosTheta = 1.0 / sqrt(1 + tanTheta2)
+        end
+        sinTheta = sqrt(max(0.0, 1.0 - cosTheta * cosTheta))
+        wh = sphereical_distribution(sinTheta, cosTheta, phi)
+        if (!same_hemisphere(wo, wh)) 
+            wh = -wh
+        end
+    else 
+        flip = wo.z < 0
+        wh = TrowbridgeReitzSample(flip ? -wo : wo, alphax, alphay, u.x, u.y)
+        if (flip) 
+            wh = -wh
+        end
+    end
+    return wh
+end
+
+function TrowbridgeReitzSample(wi::Vec3, alpha_x::Float64, alpha_y::Float64, U1::Float64, U2::Float64)
+    # 1. stretch wi
+    wiStretched = normalize(Vec3(alpha_x * wi.x, alpha_y * wi.y, wi.z))
+
+    # 2. simulate P22_{wi}(x_slope, y_slope, 1, 1)
+    slope_x, slope_y = TrowbridgeReitzSample11(cos_theta(wiStretched), U1, U2)
+
+    # 3. rotate
+    tmp = cos_phi(wiStretched) * slope_x - sin_phi(wiStretched) * slope_y
+    slope_y = sin_phi(wiStretched) * slope_x + cos_phi(wiStretched) * slope_y
+    slope_x = tmp
+
+    # 4. unstretch
+    slope_x = alpha_x * slope_x
+    slope_y = alpha_y * slope_y
+
+    # 5. compute normal
+    return normalize(Vec3(-slope_x, -slope_y, 1.0))
+end
+
+static void TrowbridgeReitzSample11(Float cosTheta, Float U1, Float U2,
+                                    Float *slope_x, Float *slope_y) {
+    // special case (normal incidence)
+    if (cosTheta > .9999) {
+        Float r = sqrt(U1 / (1 - U1));
+        Float phi = 6.28318530718 * U2;
+        *slope_x = r * cos(phi);
+        *slope_y = r * sin(phi);
+        return;
+    }
+
+    Float sinTheta =
+        std::sqrt(std::max((Float)0, (Float)1 - cosTheta * cosTheta));
+    Float tanTheta = sinTheta / cosTheta;
+    Float a = 1 / tanTheta;
+    Float G1 = 2 / (1 + std::sqrt(1.f + 1.f / (a * a)));
+
+    // sample slope_x
+    Float A = 2 * U1 / G1 - 1;
+    Float tmp = 1.f / (A * A - 1.f);
+    if (tmp > 1e10) tmp = 1e10;
+    Float B = tanTheta;
+    Float D = std::sqrt(
+        std::max(Float(B * B * tmp * tmp - (A * A - B * B) * tmp), Float(0)));
+    Float slope_x_1 = B * tmp - D;
+    Float slope_x_2 = B * tmp + D;
+    *slope_x = (A < 0 || slope_x_2 > 1.f / tanTheta) ? slope_x_1 : slope_x_2;
+
+    // sample slope_y
+    Float S;
+    if (U2 > 0.5f) {
+        S = 1.f;
+        U2 = 2.f * (U2 - .5f);
+    } else {
+        S = -1.f;
+        U2 = 2.f * (.5f - U2);
+    }
+    Float z =
+        (U2 * (U2 * (U2 * 0.27385f - 0.73369f) + 0.46341f)) /
+        (U2 * (U2 * (U2 * 0.093073f + 0.309420f) - 1.000000f) + 0.597999f);
+    *slope_y = S * z * std::sqrt(1.f + *slope_x * *slope_x);
+
+    CHECK(!std::isinf(*slope_y));
+    CHECK(!std::isnan(*slope_y));
+}
+
 
 ########################
 ######### G()
