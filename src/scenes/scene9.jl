ed28830469002e567bf846de99a91954c7b9105e
John Myslinski <jbmyslinski@gmail.com>
2025-02-06 20:45:16 -0500

    scene nine timeg

--- src/scenes/scene9.jl
+++ src/scenes/scene9.jl
@@ -9,8 +9,8 @@ function make_scene9()::Tuple{AbstractIntegrator, Scene}
         nothing
     )
     mat_inner = Matte(
-        ConstantTexture(spectrum_from_float(0.4, 0.4, 0.7)),
-        ConstantTexture(spectrum_from_float(20.0, 20.0, 20.0)),
+        ConstantTexture(spectrum_from_float(1.0, 1.0, 1.0)),
+        ConstantTexture(spectrum_from_float(0.0, 0.0, 0.0)),
         nothing
     )
 
@@ -20,7 +20,7 @@ function make_scene9()::Tuple{AbstractIntegrator, Scene}
 
     mesh012_translate = Translate(Pnt3(0,0,0)) 
     mesh0 =  parse_obj(
-        "../ref/lte-orb/mesh-0.obj",
+        "../ref/lte-orb/mesh-0.obj", # inner
         mesh012_translate,
         true,
         false,
@@ -29,26 +29,26 @@ function make_scene9()::Tuple{AbstractIntegrator, Scene}
     for tri in mesh0
         push!(primitives, Primitive(tri, mat_inner, nothing))
     end
-    mesh1 =  parse_obj(
-        "../ref/lte-orb/mesh-1.obj",
-        mesh012_translate,
-        true,
-        false,
-        nothing
-    )
-    for tri in mesh1
-        push!(primitives, Primitive(tri, mat_inner, nothing))
-    end
-    mesh2 =  parse_obj(
-        "../ref/lte-orb/mesh-2.obj",
-        mesh012_translate,
-        true,
-        false,
-        nothing
-    )
-    for tri in mesh2
-        push!(primitives, Primitive(tri, mat_inner, nothing))
-    end
+    # mesh1 =  parse_obj(
+    #     "../ref/lte-orb/mesh-1.obj", # base
+    #     mesh012_translate,
+    #     true,
+    #     false,
+    #     nothing
+    # )
+    # for tri in mesh1
+    #     push!(primitives, Primitive(tri, mat_inner, nothing))
+    # end
+    # mesh2 =  parse_obj(
+    #     "../ref/lte-orb/mesh-2.obj", # outer
+    #     mesh012_translate,
+    #     true,
+    #     false,
+    #     nothing
+    # )
+    # for tri in mesh2
+    #     push!(primitives, Primitive(tri, mat_inner, nothing))
+    # end
 
 
 
@@ -58,8 +58,8 @@ function make_scene9()::Tuple{AbstractIntegrator, Scene}
     print("Done building BVH\n")
 
     # instantiate the infinite light
-    l_2_w = Translate(Pnt3(0,0,0))
-    light = InfiniteLight(world_bounds(bvh), l_2_w, Spectrum(3.0, 3.0, 3.0), "/Users/johnmyslinski/Documents/PBRJ/scratch/mipmap/hello.exr")
+    l_2_w = Rotate(-90.0, Vec3(1,0,0))
+    light = InfiniteLight(world_bounds(bvh), l_2_w, Spectrum(1.4, 1.4, 1.4), "/Users/johnmyslinski/Documents/pbrt-v4-scenes/lte-orb/textures/small_rural_road_equiarea.exr")
     push!(lights, light)
 
     # Instantiate a Filter
@@ -76,11 +76,11 @@ function make_scene9()::Tuple{AbstractIntegrator, Scene}
     )
 
     # Instantiate a Camera
-    look_from = Pnt3(-.3, .5, -.5)
-    look_at = Pnt3(0, 0.0, 0)
+    look_from = Pnt3(0.2, 0.4, -0.5)
+    look_at = Pnt3(0, 0.1, 0)
     up = Vec3(0, 1, 0)
     screen = Bounds2(Pnt2(-1, -1), Pnt2(1, 1))
-    C = PerspectiveCamera(LookAt(look_from, look_at, up), screen, 0.0, 1.0, 0.0, 1e6, 37.0, film)
+    C = PerspectiveCamera(LookAt(look_from, look_at, up) * Scale(-1.0, 1.0, 1.0), screen, 0.0, 1.0, 0.0, 1e6, 37.0, film)
 
     # Instantiate a Sampler
     S = StratifiedSampler(parsed_args["samples-per-pixel"], parsed_args["jitter"])
