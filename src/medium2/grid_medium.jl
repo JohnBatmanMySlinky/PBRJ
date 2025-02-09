1e536df2b3c02b13ec3a849011104d35cc12bcb9
John Myslinski <jbmyslinski@gmail.com>
2025-02-08 23:19:05 -0500

    hehehe

--- src/medium2/grid_medium.jl
+++ src/medium2/grid_medium.jl
@@ -3,12 +3,13 @@ struct GridMedium <: AbstractMedium
     render_from_medium::Transformation
     sigma_a::Spectrum
     sigma_s::Spectrum
-    density_grid::SampledGrid
+    sigma_scale::Float64
     phase::AbstractPhaseFunction
-    # no temperature_grid right now
-    # no Le right now
-    # no is_emissive right now because no temp grid
-    # no temperature_scale or temperature_offset either
+    density_grid::SampledGrid
+    temperature_grid::Maybe{SampledGrid}
+    le::Spectrum
+    le_grid::Maybe{SampledGrid}
+    is_emissive::Bool
     majorant_grid::MajorantGrid
 
     function GridMedium(
@@ -17,19 +18,20 @@ struct GridMedium <: AbstractMedium
         sigma_a::Spectrum,
         sigma_s::Spectrum,
         sigma_scale::Float64=1.0,
-        p0::Pnt3=Pnt3(0,0,0),
-        p1::Pnt3=Pnt3(1,1,1),
+        le::Spectrum,
         g::Float64=0.0,
         majorant_grid_res::Pnt3=Pnt3(16, 16, 16)
     )
-        nx, ny, nz, d = parse_media(fpath)
+        nx, ny, nz, d, le_grid, temperature_grid, p0, p1 = parse_media(fpath)
         density_grid = SampledGrid(d, nx, ny, nz)
+        temperature_grid =  SampledGrid(temperature_grid, nx, ny, nz)
+        le_grid = SampledGrid(d, nx, ny, nz)
+
         majorant_grid_d = zeros(Float64, Int64(majorant_grid_res.x * majorant_grid_res.y * majorant_grid_res.z))
         for z in 0:(majorant_grid_res.z-1)
             for y in 0:(majorant_grid_res.y-1)
                 for x in 0:(majorant_grid_res.x-1)
                     tmp_bounds = voxel_bounds(majorant_grid_res, Int64(x), Int64(y), Int64(z))
-                    # print("GridBuild ($x, $y,  $z) - $tmp_bounds - $(max_value(density_grid, tmp_bounds))\n")
                     majorant_grid_d[Int64(x+majorant_grid_res.x * (y + majorant_grid_res.y * z) + 1)] = max_value(density_grid, tmp_bounds)
                 end
             end
@@ -41,8 +43,13 @@ struct GridMedium <: AbstractMedium
             Inv(medium_to_world), 
             sigma_a * sigma_scale, 
             sigma_s * sigma_scale, 
-            density_grid, 
+            sigma_scale,
             HenyeyGreenstein(g),
+            density_grid, 
+            temperature_grid,
+            le,
+            le_grid,
+            !(temperature_grid isa Nothing) || !(le_grid isa Nothing),
             majorant_grid
         )
     end
