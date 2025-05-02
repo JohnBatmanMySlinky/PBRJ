1ef8e34673bab0626e13858417423e3984269ab5
Ubuntu <jmyslinski@jmyslinski-vm4.ynfkrownol3uzdajtmr2pvym3f.bx.internal.cloudapp.net>
2025-05-02 13:12:45 +0000

    hehehehehehehe SDF tree is working

--- src/shapes/sdfs/sdf_utils.jl
+++ src/shapes/sdfs/sdf_utils.jl
@@ -2,11 +2,26 @@ function sdf_smooth_union(d1::Float64, d2::Float64, k::Float64)::Float64
     h = clamp(0.5 + 0.5 * (d2 - d1) / k, 0.0, 1.0)
     return min(d2, d1, h) - k * h * (1.0 - h)
 end
+function sdf_union(d1::Float64, d2::Float64, ::Float64)::Float64
+    return min(d1, d2)
+end
+
 function sdf_smooth_subtraction(d1::Float64, d2::Float64, k::Float64)::Float64
     h = clamp(0.5 - 0.5 * (d2 + d1) / k, 0.0, 1.0)
     return min(d2, -d1, h) + k * h * (1.0 - h)
 end
+function sdf_subtraction(d1::Float64, d2::Float64, ::Float64)::Float64
+    return max(-d1, d2)
+end
+
 function sdf_smooth_intersection(d1::Float64, d2::Float64, k::Float64)::Float64
     h = clamp(0.5 - 0.5 * (d2 - d1)/k, 0.0, 1.0)
     return min(d2, d1, h) + k * h * (1.0 - h)
+end
+function sdf_intersection(d1::Float64, d2::Float64, ::Float64)::Float64
+    return max(d1, d2)
+end
+
+function sdf_xor(d1::Float64, d2::Float64, ::Float64)::Float64
+    return max(min(d1,d2), -max(d1,d2))
 end
