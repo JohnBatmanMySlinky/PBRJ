18bdd030822f4119bd9f5f6068e9d6164cbdf7fc
John Myslinski <jbmyslinski@gmail.com>
2025-06-14 12:08:50 -0400

    cleanup

--- src/integrators/integrator.jl
+++ src/integrators/integrator.jl
@@ -1,5 +1,5 @@
 function render(
-    i::Union{WhittedIntegrator, PathIntegrator, AOIntegrator, SimpleIntegrator, SimpleVolPathIntegrator}, 
+    i::Union{AOIntegrator, SimpleIntegrator, SimpleVolPathIntegrator}, 
     scene::Scene, 
     ::Dict{String, Any},
     ::Tuple{Int64,Int64}  
