402678cc6da70484c66ce10c4e00ef1b5189bd75
John Myslinski <jbmyslinski@gmail.com>
2025-10-19 14:58:31 -0400

    need to do beam diffusion first so array is empty so it dont inf loop

--- src/reflection/bssrdf.jl
+++ src/reflection/bssrdf.jl
@@ -16,6 +16,35 @@ struct BSSRDFTable
         profile = zeros(Float64, n_rho_samples * n_radius_samples)
         rho_eff = zeros(Float64, n_rho_samples)
         profile_cdf = zeros(Float64, n_rho_samples * n_radius_samples)
+
+        # Choose radius values of the diffusion profile discretization
+        radius_samples[0 + 1] = 0
+        radius_samples[1 + 1] = 2.5e-3
+        for i in 2:(n_radius_samples - 1)
+            radius_samples[i + 1] = radius_samples[i - 1 + 1] * 1.2
+        end
+
+        # Choose albedo values of the diffusion profile discretization
+        for i in 0:(n_rho_samples - 1)
+            rho_samples[i + 1] = (1.0 - exp(-8.0 * i / (n_rho_samples - 1))) / (1.0 - exp(-8.0))
+            # Compute the diffusion profile for the _i_th albedo sample
+            # Compute scattering profile for chosen albedo $\rho$
+            for j in 0:(n_radius_samples - 1)
+                rho = rho_samples[i + 1]
+                r = radius_samples[j + 1]
+                profile[i * n_radius_samples + j + 1] = 2 * pi * r * (
+                    beam_diffusion_ss(rho, 1.0 - rho, g, eta, r) + 
+                    beam_diffusion_ms(rho, 1.0 - rho, g, eta, r1)
+                )
+            end
+
+            rho_eff[i + 1] = integrate_catmull_rom(
+                n_radius_samples,
+                radius_samples,
+                profile[i * n_radius_samples + 1],
+                profile_cdf[i * n_radius_samples + 1]
+            )
+        end
         return new(n_rho_samples, n_radius_samples, rho_samples, radius_samples, profile, rho_eff, profile_cdf)
     end
 end
